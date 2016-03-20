35b1c8070d9e1bc32d72023f165a483ed564e968
Paul Apivat <paul.apivat@gmail.com>
2016-03-20 13:08:49 +0700

    add random color button

--- project2.js
+++ project2.js
@@ -7,3 +7,13 @@
 $(document).ready(function(){
 	$('#drag').draggable();
 });
+
+$(document).ready(function(){
+		function randomColor(){
+			return '#' + Math.random().toString(16).slice(2,8);
+		};
+
+	$('button').click(function(){
+		$('body').css('background', randomColor());
+	});
+});
