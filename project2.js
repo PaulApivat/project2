2b5cedde63bf493a75504f55f477efd60a740977
Paul Apivat <paul.apivat@gmail.com>
2016-03-20 13:39:51 +0700

    Add JQuery UI locally, random color button

--- project2.js
+++ project2.js
@@ -1,19 +1,35 @@
-//$(document).ready(function(){
-//	$('div').dblclick(function(){
-//			$(this).fadeOut('fast');
-//		});
-//});
+$(document).ready(function(){
+	$('#fade').dblclick(function(){
+			$(this).fadeOut('fast');
+		});
+});
 
 $(document).ready(function(){
 	$('#drag').draggable();
 });
 
+//code for getting random color function returns '#' + random number, passed with string 16 decimals and sliced half?
+
+// $(document).ready(function(){
+//		function randomColor(){
+//			return '#' + Math.random().toString(16).slice(2,8);
+//		};
+
+//	$('#button').click(function(){
+//		$('body').css('background', randomColor());
+//	});
+//  });
+
+
+//Try another version from StackOverflow ALSO WORKS!!!!!!
+
 $(document).ready(function(){
-		function randomColor(){
-			return '#' + Math.random().toString(16).slice(2,8);
-		};
+	function getRandomColor() {
+		var hex = Math.floor(Math.random() * 0xFFFFFF);
+		return "#" + ("000000" + hex.toString(16)).substr(-6);
+	};
 
 	$('#button').click(function(){
-		$('body').css('background', randomColor());
+		$('body').css('background', getRandomColor());
 	});
 });
